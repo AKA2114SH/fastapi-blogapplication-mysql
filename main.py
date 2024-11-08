2559c1e13cd4790c3efa2420523c190e2360ca97
Akash <akashkhatale2114@gmail.com>
2024-11-08 14:48:03 +0530

    add Query parameters

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.get('/')
+@app.get("/", summary="Index")
 def index():
-    return {'data':{"Name":"Akash"}}
+    return {'data': 'blog list'}
 
+@app.get("/blog/unpublished", summary="Unpublished")
+def unpublish():
+    return {'data': 'all unpublished blogs'}
 
-@app.get('/about')
-def about():
-    return {'data':{"about page"}}
+@app.get("/blog/{id}", summary="Show")
+def show(id: int):
+    # Fetch blog with id = id
+    return {'data': id}
+
+@app.get("/blog/{id}/comments", summary="Comments")
+def comments(id: int):
+    # Fetch comments of blog with id = id
+    return {'data': ['1', '2']}  # Example response
