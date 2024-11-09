57355fde45d94d91bfede8f3ffaf6773edbe5ffa
Akash <akashkhatale2114@gmail.com>
2024-11-09 11:37:25 +0530

    Request Body (POST)

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ app = FastAPI()
 def index():
     return {'data': 'blog list'}
 
-@app.get("/blog/unpublished", summary="Unpublished")
+# Only get limited number of published or all blogs
+@app.get("/blog", summary="Blog List")
+def get_blogs(limit: int=10 , published: bool=True,sort:str=None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+@app.get("/blog/unpublished", summary="Unpublished Blogs")
 def unpublish():
     return {'data': 'all unpublished blogs'}
 
-@app.get("/blog/{id}", summary="Show")
+@app.get("/blog/{id}", summary="Show Blog")
 def show(id: int):
     # Fetch blog with id = id
     return {'data': id}
 
-@app.get("/blog/{id}/comments", summary="Comments")
-def comments(id: int):
+@app.get("/blog/{id}/comments", summary="Blog Comments")
+def comments(id: int,limit=10):
     # Fetch comments of blog with id = id
     return {'data': ['1', '2']}  # Example response
